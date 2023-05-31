8c076fd637cd153e60581e94de68803aae7fad25
yangshaojun <yangshaojun@zhidaoauto.com>
2023-05-31 16:57:46 +0800

    集合操作

---
+++ dophin-flink-1.17.0/src/main/java/com/netcloud/bigdata/flink/sql/_03sqlgrammar/dml/_08_set_operations/IntersectExample.java
@@ -0,0 +1,58 @@
+package com.netcloud.bigdata.flink.sql._03sqlgrammar.dml._08_set_operations;
+
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.TableEnvironment;
+
+import java.util.Arrays;
+
+/**
+ * @author netcloud
+ * @date 2023-05-31 16:44:29
+ * @email netcloudtec@163.com
+ * @description
+ */
+public class IntersectExample {
+    public static void main(String[] args) {
+        EnvironmentSettings settings = EnvironmentSettings
+                .newInstance()
+                .inStreamingMode() //声明为流任务
+                //.inBatchMode()//声明批流任务
+                .build();
+        TableEnvironment tableEnv = TableEnvironment.create(settings);
+
+        String sql = "CREATE TABLE source_table_1 (\n"
+                + "    user_id BIGINT NOT NULL\n"
+                + ") WITH (\n"
+                + "  'connector' = 'datagen',\n"
+                + "  'rows-per-second' = '10',\n"
+                + "  'fields.user_id.min' = '1',\n"
+                + "  'fields.user_id.max' = '10'\n"
+                + ");\n"
+                + "\n"
+                + "CREATE TABLE source_table_2 (\n"
+                + "    user_id BIGINT NOT NULL\n"
+                + ") WITH (\n"
+                + "  'connector' = 'datagen',\n"
+                + "  'rows-per-second' = '10',\n"
+                + "  'fields.user_id.min' = '1',\n"
+                + "  'fields.user_id.max' = '10'\n"
+                + ");\n"
+                + "\n"
+                + "CREATE TABLE sink_table (\n"
+                + "    user_id BIGINT\n"
+                + ") WITH (\n"
+                + "  'connector' = 'print'\n"
+                + ");\n"
+                + "\n"
+                + "INSERT INTO sink_table\n"
+                + "SELECT user_id\n"
+                + "FROM source_table_1\n"
+                + "INTERSECT\n"
+                + "SELECT user_id\n"
+                + "FROM source_table_2\n";
+
+        Arrays.stream(sql.split(";"))
+                .forEach(tableEnv::executeSql);
+
+    }
+}
